d3fc438e16b22003b32e6cea67e4e944e05de49a
Benjamin Maitland <marthinwurer@gmail.com>
2019-08-21 23:00:54 -0400

    Finished tests for 153

--- tests/74x153/tb_74x153.v
+++ tests/74x153/tb_74x153.v
@@ -21,8 +21,8 @@ module tb_74x153 ();
 
     // declare our test data
     parameter inbetween = 50;
-    parameter num_tests = 32;
-    reg [13:0] test_info[0:1];
+    parameter num_tests = 19;
+    reg [13:0] test_info[num_tests];
     reg[31:0] test;
 
     // module under test
@@ -46,8 +46,28 @@ module tb_74x153 ();
     initial begin
         $dumpfile("tb_74x153.vcd");
         $dumpvars(0,to_test);
-        test_info[1] = 14'b00000000000000;
-        test_info[2] = 14'b00000000000000;
+        //                      c1  c2  yy
+        //                  abgg0123012312
+        test_info[0] <= 14'b00000000000000;
+        test_info[1] <= 14'b00001000000010;
+        test_info[2] <= 14'b10001000000000;
+        test_info[3] <= 14'b10001100000010;
+        test_info[4] <= 14'b01001100000000;
+        test_info[5] <= 14'b01001110000010;
+        test_info[6] <= 14'b11001110000000;
+        test_info[7] <= 14'b11001111000010;
+        test_info[8] <= 14'b00001111000010;
+        test_info[9] <= 14'b00001111100011;
+        test_info[10]<= 14'b10001111100010;
+        test_info[11]<= 14'b10001111110011;
+        test_info[12]<= 14'b01001111110010;
+        test_info[13]<= 14'b01001111111011;
+        test_info[14]<= 14'b11001111111010;
+        test_info[15]<= 14'b11001111111111;
+        test_info[16]<= 14'b11111111111100;
+        test_info[17]<= 14'b11011111111110;
+        test_info[18]<= 14'b11101111111101;
+//        #inbetween;
         for (test = 0; test < num_tests; test = test + 1) begin
             // assign
             {a, b, enable1, enable2, c10, c11, c12, c13, c20, c21, c22, c23} <= test_info[test][13:2];
@@ -55,7 +75,12 @@ module tb_74x153 ();
             #inbetween;
             // assert
 //            assert ({y1, y2} == test_info[1:0]);
+            if ({y1, y2} != test_info[test][1:0]) begin
+                $display("Assertation failed at test %h", test);
+            end
         end
+        #inbetween;
+        $finish;
     end
 
 endmodule
